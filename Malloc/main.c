88090b6c8230a7a9b8f640eb9a801759d2e7ae73
Faisal786111 <faisalkhanisrar@gmail.com>
2025-06-17 20:27:09 +0530

    Added more stuff in malloc

--- Malloc/main.c
+++ Malloc/main.c
@@ -8,11 +8,17 @@
  * Parameters:
  *  size: Number of bytes to allocate
  *  return_type : Void pointer (generic pointer) Which can be typecasted to other types
+ * Memory Deallocation:
+ *  void *free(void *ptr)
+ * Parameters:
+ *  ptr is the pointer to the memory block that needs to be freed or deallocated.
+ *  return_type : Void pointer (generic pointer) Which can be typecasted to other 
  */
 
 
 int main(){
 
+    // Malloc based string
     char s[20]; // Memory allocated on stack (fixed size)
 
     // Dynamic memory allocation 
@@ -24,6 +30,20 @@ int main(){
 
     // After using free up the momery 
     free(str);
+
+    // Malloc based array 
+    int n;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
     
     return 0;
 }
