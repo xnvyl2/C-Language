2812747f244f6ee07e00b5c0732746598eb9303c
Faisal786111 <faisalkhanisrar@gmail.com>
2025-06-18 11:10:33 +0530

    Min_three completed

---
+++ Problems/Min_three/main.c
@@ -0,0 +1,32 @@
+#include "../Common/headers.h"
+
+/**
+ * Problem: 
+ * Smallest number between 3 numbers.
+ */
+
+int min(int count, ...){
+    va_list args;
+
+    va_start(args, count);
+
+    int min = INT_MAX, i;
+
+    for(i = 0; i < count; i++){
+        int x = va_arg(args, int);
+        if(x < min){
+            min = x;
+        }
+    }
+
+    va_end(args);
+    return min;
+}
+
+
+int main() {
+
+    printf("Min : %d", min(4, 2, 30, 0, 1));
+
+    return 0;
+}
